c2ad6ad253a2175a2ef093edb984ca613ddf0240
YairVera <yairvera44@gmail.com>
2025-10-10 13:03:35 -0500

    initial push

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+
 public class Application {
+    public static void main(String[] args) {
+
+
+
+    }
 }
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,6 @@
 package com.pluralsight;
 
 public class Transactions {
+
+
 }
