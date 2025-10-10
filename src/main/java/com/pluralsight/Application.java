040814146d50197a7c0cda05d5a2af3d5d30e67d
YairVera <yairvera44@gmail.com>
2025-10-10 14:53:34 -0500

    constructor, getter and setters

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -20,56 +20,41 @@ public class Application {
         ArrayList<Transaction> transactions = new ArrayList<>();
 
         try {
-            // create a FileReader object connected to the File
+
             FileReader fileReader = new FileReader("transactions.csv");
-            // create a BufferedReader to manage input stream
+
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
-            //skip header line
             bufferedReader.readLine();
 
-            // read until there is no more data
             while ((line = bufferedReader.readLine()) != null) {
-//                System.out.println(line);
+
                 String[] parts = line.split(Pattern.quote("|"));
                 System.out.println(line);
 
+                Transaction transaction = new Transaction();
 
-//                Employee employee = new Employee();
-//
 //                String employeeIdAsString = parts[0];
 //                int employeeId = Integer.parseInt(employeeIdAsString);
-//                employee.setEmployeeId(employeeId);
-////                OR
-////                  employee.setEmployeeId(Integer.parseInt(parts[0]));
+//                transactions.setEmployeeId(employeeId);
 //
 //                String name = parts[1];
-//                employee.setName(name);
-////                OR
-////                employee.setName(parts[1]);
-//
+//                transactions.setName(name);
 //
 //                String hoursWorkedAsString = parts[2];
 //                double hoursWorked = Double.parseDouble(hoursWorkedAsString);
-//                employee.setHoursWorked(hoursWorked);
-////                OR
-////                employee.setHoursWorked(Double.parseDouble(parts[2]));
-//
+//                transactions.setHoursWorked(hoursWorked);
 //
 //                String payRateAsString = parts[3];
 //                double payRate = Double.parseDouble(payRateAsString);
-//                employee.setPayRate(payRate);
-////                OR
-////                employee.setPayRate(Double.parseDouble(parts[3]));
-//
-//                employees.add(employee) ;
-////                System.out.println(employee);
+//                transactions.setPayRate(payRate);
+
+                transactions.add(transaction) ;
+
             }
-            // close the stream and release the resources
             bufferedReader.close();
         } catch (IOException e) {
-            // display stack trace if there was an error
             e.printStackTrace();
         }
         return transactions;
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,69 @@
 package com.pluralsight;
 
 public class Transaction {
+
+    private String transactionDate;
+    private String itemType;
+    private String descriptions;
+    private String vendorName;
+    private double itemPrice;
+
+    public Transaction() {
+        this.transactionDate = "";
+        this.itemType = "";
+        this.descriptions = "";
+        this.vendorName = "";
+        this.itemPrice = 0;
+    }
+
+//constructor
+    public Transaction(String transactionDate, String itemType, String descriptions, String vendorName, double itemPrice) {
+        this.transactionDate = transactionDate;
+        this.itemType = itemType;
+        this.descriptions = descriptions;
+        this.vendorName = vendorName;
+        this.itemPrice = itemPrice;
+    }
+
+//getters and setters
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
 }
